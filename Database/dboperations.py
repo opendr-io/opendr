efacea322c967bcde20bb8ffb942db7c80dfa5c1
randomuserid <randomuserid@users.noreply.github.com>
2025-03-27 14:47:56 -0400

    database components
    initial commit of database components

---
+++ Database/dboperations.py
@@ -0,0 +1,55 @@
+import os
+import time
+import shutil
+import schedule
+from pathlib import Path
+from storedata import StoreData
+
+def directory_cleanup():
+  directory = 'done/ready'
+  with os.scandir(directory) as files:
+    for file in files:
+      if(file.is_file()):
+        os.unlink(file.path)
+
+def monitor_directory(dir, pat):
+  dataStorage = StoreData()
+  path = Path(dir)
+  processed_files = set()
+  schedule.every(30).minutes.do(directory_cleanup)
+  while True:
+    try:
+      current_files = set(path.glob(pat))
+      new_files = current_files - processed_files
+      if not new_files:
+        time.sleep(30)
+        continue            
+      for new_file in new_files:
+        fn = str(new_file)
+        file_type = None
+        if('process' in fn):
+          dataStorage.store_process_events(fn)
+        elif('network' in fn):
+          dataStorage.store_network_events(fn)
+        elif('services' in fn):
+          dataStorage.store_installed_services(fn)
+        elif('software' in fn):
+          dataStorage.store_installed_applications(fn)
+        elif('endpoint' in fn):
+          dataStorage.store_endpoint_info(fn)
+        elif('users' in fn):
+          dataStorage.store_user_info(fn)
+        processed_files.add(new_file)
+        schedule.run_pending()
+        shutil.move(fn, 'done/'+fn)
+    except Exception as e:
+      print(f"Error: {e}")
+      time.sleep(1)
+      
+def run():
+  os.makedirs('done/ready', exist_ok=True)
+  directory = 'ready'
+  pat = '*.log'
+  monitor_directory(directory, pat)
+
+run()
---
+++ Database/dbsetup.py
@@ -0,0 +1,35 @@
+import psycopg
+from pathlib import Path
+from storedata import StoreData
+
+def setup_postgres_tables():
+  applications_table = 'applications(timestmp, hostname, sid, ec2instanceid, program, servicename, displayname, status, start, username, pid, executable)'
+  system_events_table = 'systemevents(timestmp, hostname, event, pid, process, sid, username, executable, commandline, dnsname, dnsdate, sourceip, sourceport, destip, destport, asname, status, onterminal, fromhostname, logintime)'
+  print('Initiating Database Connection')
+  with psycopg.connect() as connection:
+    print('Connection Made!')
+    with connection.cursor() as cursor:
+      print('Creating PostgreSQL Database')
+      cursor.execute("CREATE DATABASE opendr")
+      print('Database Created!')
+    connection.commit()
+
+  with psycopg.connect() as connection:
+    with connection.cursor() as cursor:
+      print('Creating PostgreSQL Tables and Users')
+      print('Users Created!')
+      cursor.execute("""CREATE TABLE applications (id serial PRIMARY KEY, timestmp text, hostname text, ec2instanceid text, program text, servicename text, displayname text, status text, start text, username text, pid TEXT, executable text, sid text)""")
+      cursor.execute("""CREATE TABLE endpointinfo (id serial PRIMARY KEY, timestmp text, hostname text,  ec2instanceid text, privateips text, publicip text, event text, username text,
+      onterminal text, fromhostname text, logintime text, sid text)""")
+      cursor.execute("""CREATE TABLE systemevents (id serial PRIMARY KEY, timestmp text, event text, pid integer, name text, hostname text, ppid text,
+      parent text, username text, dnsname text, dnsdate text, sourceip text, sourceport text, destip text, destport text, asname text, status text, sid text)""")
+      print('Tables Created!')
+      cursor.execute("CREATE USER agent WITH PASSWORD 'Agent!123'")
+      cursor.execute("CREATE USER app WITH PASSWORD 'user!123'")
+      cursor.execute("GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO agent;")
+      cursor.execute("GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO app")
+      cursor.execute("GRANT INSERT ON ALL TABLES IN SCHEMA public TO agent")
+      cursor.execute("GRANT SELECT ON ALL TABLES IN SCHEMA public TO app")
+    connection.commit()
+
+setup_postgres_tables()
