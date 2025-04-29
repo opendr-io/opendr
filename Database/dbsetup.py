0fc833f62b57005ebf0f21667fb570ce90897a34
Alan Koshy <alankoshy16@gmail.com>
2025-04-29 20:37:34 +0530

    minor fix

--- Database/dbsetup.py
+++ Database/dbsetup.py
@@ -22,19 +22,19 @@ def setup_postgres_tables():
                       user=config.get('Database', 'RootDatabaseUserName', fallback='postgres'), password=config.get('Database', 'RootDatabasePassword')) as connection:
     with connection.cursor() as cursor:
       print('Creating PostgreSQL Tables and Users')
-      print('Users Created!')
       cursor.execute("""CREATE TABLE endpointinfo (id serial PRIMARY KEY, timestmp text, hostname text, ec2instanceid text, privateips text, publicip text, event text, username text,
         onterminal text, fromhostname text, logintime text, sid text)""")
       cursor.execute("""CREATE TABLE systemevents (id serial PRIMARY KEY, timestmp text, event text, pid text, name text, hostname text, ppid text,
         parent text, username text, dnsname text, dnsdate text, sourceip text, sourceport text, destip text, destport text, asname text, status text, 
         onterminal text, fromhostname text, logintime text, sid text)""")
       print('Tables Created!')
-      cursor.execute(f"CREATE USER {config.get('Database', 'AgentUserName', fallback='agent')} WITH PASSWORD {config.get('Database', 'AgentPassword')}")
-      cursor.execute(f"CREATE USER {config.get('Database', 'AppUserName', fallback='app')} WITH PASSWORD {config.get('Database', 'AppUserPassword')}")
+      cursor.execute(f"CREATE USER {config.get('Database', 'AgentUserName', fallback='agent')} WITH PASSWORD '{config.get('Database', 'AgentPassword')}'")
+      cursor.execute(f"CREATE USER {config.get('Database', 'AppUserName', fallback='app')} WITH PASSWORD '{config.get('Database', 'AppUserPassword')}'")
       cursor.execute(f"GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO {config.get('Database', 'AgentUserName', fallback='agent')}")
       cursor.execute(f"GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO {config.get('Database', 'AppUserName', fallback='app')}")
       cursor.execute(f"GRANT INSERT ON ALL TABLES IN SCHEMA public TO {config.get('Database', 'AgentUserName', fallback='agent')}")
       cursor.execute(f"GRANT SELECT ON ALL TABLES IN SCHEMA public TO {config.get('Database', 'AppUserName', fallback='app')}")
+      print('Users Created!')
     connection.commit()
 
 setup_postgres_tables()
