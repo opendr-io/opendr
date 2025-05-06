8a832c30a2119423a5b2fe8211b7644f0475ba6c
Alan Koshy <alankoshy16@gmail.com>
2025-05-06 22:25:11 +0530

    added uniform logging and db shipping method

--- Database/storedata.py
+++ Database/storedata.py
@@ -110,7 +110,7 @@ class StoreData:
           sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
           connection.execute(sqlInsertStatement, final_params)
           connection.commit()
-  
+
   def store_user_info(self, filename):
     table = 'systemevents(timestmp, event, pid, name, hostname, ppid, parent, username, dnsname, dnsdate, sourceip, sourceport, destip, destport, asname, status, sid)'
     with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
@@ -125,6 +125,24 @@ class StoreData:
           final_params = [data.get('timestamp'), data.get('event'), '', '', data.get('hostname'), '', '',
                           data.get('username'), '', '', data.get('sourceip'), '', '', '', '', '', data.get('sid')]
           fillers = ("%s," * 17)[:-1]
+          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+          connection.execute(sqlInsertStatement, final_params)
+          connection.commit()
+
+  def store_hotfix_info(self, filename):
+    table = 'endpointinfo(timestmp, event, hostname, ec2instanceid, privateips, publicip, username, onterminal, fromhostname, logintime, sid)'
+    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+      with open(filename, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+          if(not line):
+            continue
+          pattern = r'(\w+):([^|]+)(?:[,|]|$)'
+          matches = re.findall(pattern, line)
+          data = {key.strip(): value.strip() for key, value in matches}
+          final_params = [data.get('timestamp'), data.get('event'), data.get('hostname'), '', '', '', data.get('username'), '', '', '', '']
+          fillers = ("%s," * 11)[:-1]
+          print(final_params)
           sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
           connection.execute(sqlInsertStatement, final_params)
           connection.commit()
--- Windows/hotfixes.py
+++ Windows/hotfixes.py
@@ -1,58 +1,74 @@
 import subprocess
 import pandas as pd
 import json
-
-# PowerShell command to get hotfixes and convert to JSON
-command = [
-    "powershell",
-    "-Command",
-    "Get-HotFix | Select-Object HotFixID, Description, InstalledOn, InstalledBy, PSComputerName | ConvertTo-Json"
-]
-result = subprocess.run(command, capture_output=True, text=True, encoding="utf-8")
-
-# JSON  to DataFrame
-try:
-    hotfix_data = json.loads(result.stdout)
-    
-    # If only one item is returned, make it a list
-    if isinstance(hotfix_data, dict):
-        hotfix_data = [hotfix_data]
-
-    dfh = pd.DataFrame(hotfix_data)
-    print(dfh.head())
-except json.JSONDecodeError as e:
-    print("JSON parse error:", e)
-    print(result.stdout)
-
-    # If InstalledOn is a dictionary, extract the 'DateTime' field
-dfh['InstalledOn'] = dfh['InstalledOn'].apply(lambda x: x.get('DateTime') if isinstance(x, dict) else x)
-dfh = dfh.sort_values(by='InstalledOn', ascending=False)
-
-dfh = dfh.rename(columns={
-    "InstalledOn": "timestamp",
-    "HotFixID": "name",
-    "PSComputerName": "hostname",
-    "InstalledBy": "username",
-    "Description": "description"
-})
-
-desired_order = [
-    "timestamp",
-    "name",
-    "hostname",
-    "username",
-    "description"
-]
-dfh = dfh[[col for col in desired_order if col in dfh.columns]]
-dfh["event"] = "hotfix"
+import os
+import time
+import common.attributes as attr
+import common.logger as logfunc
 
 # format output
 def format_row_with_keys(row):
     return " | ".join(f"{col}: {row[col]}" for col in row.index if pd.notna(row[col]))
-lines = dfh.apply(format_row_with_keys, axis=1)
 
-with open("hotfix-inventory.txt", "w", encoding="utf-8") as f:
+def fetch_hotfixes(log_directory, ready_directory):
+    logger = logfunc.setup_logging(log_directory, ready_directory, "HotfixMonitor", "hotfix")
+
+    # PowerShell command to get hotfixes and convert to JSON
+    command = [
+        "powershell",
+        "-Command",
+        "Get-HotFix | Select-Object HotFixID, Description, InstalledOn, InstalledBy, PSComputerName | ConvertTo-Json"
+    ]
+    result = subprocess.run(command, capture_output=True, text=True, encoding="utf-8")
+
+    # JSON  to DataFrame
+    try:
+        hotfix_data = json.loads(result.stdout)
+        # If only one item is returned, make it a list
+        if isinstance(hotfix_data, dict):
+            hotfix_data = [hotfix_data]
+
+        dfh = pd.DataFrame(hotfix_data)
+    except json.JSONDecodeError as e:
+        print("JSON parse error:", e)
+        print(result.stdout)
+
+    # If InstalledOn is a dictionary, extract the 'DateTime' field
+    dfh['InstalledOn'] = dfh['InstalledOn'].apply(lambda x: x.get('DateTime') if isinstance(x, dict) else x)
+    dfh = dfh.sort_values(by='InstalledOn', ascending=False)
+
+    dfh = dfh.rename(columns={
+        "InstalledOn": "timestamp",
+        "HotFixID": "name",
+        "PSComputerName": "hostname",
+        "InstalledBy": "username",
+        "Description": "description"
+    })
+
+    desired_order = [
+        "timestamp",
+        "name",
+        "hostname",
+        "username",
+        "description"
+    ]
+    dfh = dfh[[col for col in desired_order if col in dfh.columns]]
+    dfh["event"] = "hotfix"
+    lines = dfh.apply(format_row_with_keys, axis=1)
     for line in lines:
-        f.write(line + "\n")
-# for debugging, can be removed
-print(f"✅ Exported {len(dfh)} rows to hotfix-inventory.txt")
+        logger.info(line)
+
+def run():
+    interval = attr.get_config_value('Windows', 'HotfixInterval', 43200.0, 'float')
+    log_directory = 'tmp-windows-hotfixes'
+    ready_directory = 'ready'
+    debug_generator_directory = 'debuggeneratorlogs'
+    os.makedirs(debug_generator_directory, exist_ok=True)
+    os.makedirs(log_directory, exist_ok=True)
+    os.makedirs(ready_directory, exist_ok=True)
+    print('hotfixlog running')
+    while True:
+        fetch_hotfixes(log_directory, ready_directory)
+        time.sleep(interval)  # Twice a day by default, can be increased or decreased
+
+run()
