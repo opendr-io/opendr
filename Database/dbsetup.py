6d0c654eb4dccaff820a48ef1cb2ccf54f5dc00c
anidev95 <130506893+anidev95@users.noreply.github.com>
2025-05-06 11:55:09 -0400

    Separating out sections of dbsetup to be more modular. One for the database creation, one for the table creation, and one for user creation

--- Database/dbsetup.py
+++ Database/dbsetup.py
@@ -1,39 +1,35 @@
 import psycopg
-import configparser
-import pathlib
-
-config = configparser.ConfigParser()
-config.read(pathlib.Path(__file__).parent.absolute() / "../dbconfig.ini")
+from pathlib import Path
 
 def setup_postgres_tables():
-  applications_table = 'applications(timestmp, hostname, sid, ec2instanceid, program, servicename, displayname, status, start, username, pid, executable)'
-  system_events_table = 'systemevents(timestmp, hostname, event, pid, process, sid, username, executable, commandline, dnsname, dnsdate, sourceip, sourceport, destip, destport, asname, status, onterminal, fromhostname, logintime)'
   print('Initiating Database Connection')
-  with psycopg.connect(host=config.get('Database', 'HostName'), port=config.get('Database', 'PortNumber', fallback='4000'), user=config.get('Database', 'RootDatabaseUserName', fallback='postgres'), 
-                      password=config.get('Database', 'RootDatabasePassword'), autocommit=True) as connection:
+  with psycopg.connect() as connection:
     print('Connection Made!')
     with connection.cursor() as cursor:
       print('Creating PostgreSQL Database')
-      cursor.execute(f"CREATE DATABASE {config.get('Database', 'DatabaseName', fallback='opendr')}")
+      cursor.execute("CREATE DATABASE opendr")
       print('Database Created!')
     connection.commit()
 
-  with psycopg.connect(host=config.get('Database', 'HostName'), port=config.get('Database', 'PortNumber', fallback='4000'), dbname=config.get('Database', 'DatabaseName', fallback='opendr'),
-                      user=config.get('Database', 'RootDatabaseUserName', fallback='postgres'), password=config.get('Database', 'RootDatabasePassword')) as connection:
+  with psycopg.connect() as connection:
     with connection.cursor() as cursor:
       print('Creating PostgreSQL Tables and Users')
-      cursor.execute("""CREATE TABLE endpointinfo (id serial PRIMARY KEY, timestmp text, hostname text, ec2instanceid text, privateips text, publicip text, event text, username text,
-        onterminal text, fromhostname text, logintime text, sid text)""")
-      cursor.execute("""CREATE TABLE systemevents (id serial PRIMARY KEY, timestmp text, event text, pid text, name text, hostname text, ppid text,
-        parent text, username text, dnsname text, dnsdate text, sourceip text, sourceport text, destip text, destport text, asname text, status text, 
-        onterminal text, fromhostname text, logintime text, sid text)""")
+      print('Users Created!')
+      cursor.execute("""CREATE TABLE applications (id serial PRIMARY KEY, timestmp text, hostname text, ec2instanceid text, program text, servicename text, displayname text, status text, start text, username text, pid TEXT, executable text, sid text)""")
+      cursor.execute("""CREATE TABLE endpointinfo (id serial PRIMARY KEY, timestmp text, hostname text,  ec2instanceid text, privateips text, publicip text, event text, username text,
+      onterminal text, fromhostname text, logintime text, sid text)""")
+      cursor.execute("""CREATE TABLE systemevents (id serial PRIMARY KEY, timestmp text, event text, pid integer, name text, hostname text, ppid text,
+      parent text, username text, dnsname text, dnsdate text, sourceip text, sourceport text, destip text, destport text, asname text, status text, sid text)""")
       print('Tables Created!')
-      cursor.execute(f"CREATE USER {config.get('Database', 'AgentUserName', fallback='agent')} WITH PASSWORD '{config.get('Database', 'AgentPassword')}'")
-      cursor.execute(f"CREATE USER {config.get('Database', 'AppUserName', fallback='app')} WITH PASSWORD '{config.get('Database', 'AppUserPassword')}'")
-      cursor.execute(f"GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO {config.get('Database', 'AgentUserName', fallback='agent')}")
-      cursor.execute(f"GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO {config.get('Database', 'AppUserName', fallback='app')}")
-      cursor.execute(f"GRANT INSERT ON ALL TABLES IN SCHEMA public TO {config.get('Database', 'AgentUserName', fallback='agent')}")
-      cursor.execute(f"GRANT SELECT ON ALL TABLES IN SCHEMA public TO {config.get('Database', 'AppUserName', fallback='app')}")
+
+  with psycopg.connect() as connection:
+    with connection.cursor() as cursor:
+      cursor.execute("CREATE USER agent WITH PASSWORD 'Agent!123'")
+      cursor.execute("CREATE USER app WITH PASSWORD 'user!123'")
+      cursor.execute("GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO agent;")
+      cursor.execute("GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO app")
+      cursor.execute("GRANT INSERT ON ALL TABLES IN SCHEMA public TO agent")
+      cursor.execute("GRANT SELECT ON ALL TABLES IN SCHEMA public TO app")
       print('Users Created!')
     connection.commit()
 
