272712f4cbb726a482f75b45fc02cd17852a54fc
Alan Koshy <alankoshy16@gmail.com>
2025-05-15 20:01:14 +0530

    Simplified local mode logging

--- Linux/package-inventory.py
+++ Linux/package-inventory.py
@@ -87,7 +87,7 @@ def log_data(log_directory, ready_directory):
         time.sleep(interval)  # Log every 60 minutes - or choose an interval
 
 def run():
-    log_directory = 'tmp-software-inventory'
+    log_directory = 'tmp-software-inventory' if attr.get_config_value('Linux', 'RunDatabaseOperations', False, 'bool') else 'tmp'
     ready_directory = 'ready'
     debug_generator_directory = 'debuggeneratorlogs'
     os.makedirs(debug_generator_directory, exist_ok=True)
