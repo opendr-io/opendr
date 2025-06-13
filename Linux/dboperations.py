07935b4a4976b8b8cc14bbaadfc946aeede925eb
anidev95 <130506893+anidev95@users.noreply.github.com>
2025-06-13 10:01:54 -0400

    Adding ability to log UUID in macOS

---
+++ Linux/dboperations.py
@@ -0,0 +1,66 @@
+import os
+import time
+import shutil
+import schedule
+from pathlib import Path
+from storedata import StoreData
+import configparser
+import pathlib
+
+config = configparser.ConfigParser()
+config.read(pathlib.Path(__file__).parent.absolute() / "../dbconfig.ini")
+
+db_interval = config.getint('Database', 'DatabaseInterval', fallback=30)
+cleanup_interval = config.getint('Database', 'CleanupInterval', fallback=30)
+
+def directory_cleanup():
+  directory = 'done/ready'
+  with os.scandir(directory) as files:
+    for file in files:
+      if(file.is_file()):
+        os.unlink(file.path)
+
+def monitor_directory(dir, pat):
+  dataStorage = StoreData()
+  path = Path(dir)
+  processed_files = set()
+  schedule.every(cleanup_interval).minutes.do(directory_cleanup)
+  while True:
+    try:
+      current_files = set(path.glob(pat))
+      new_files = current_files - processed_files
+      if not new_files:
+        time.sleep(db_interval)
+        continue            
+      for new_file in new_files:
+        fn = str(new_file)
+        if ('process' in fn):
+          dataStorage.store_process_events(fn)
+        elif ('network' in fn):
+          dataStorage.store_network_events(fn)
+        elif ('services' in fn):
+          dataStorage.store_installed_services(fn)
+        elif ('software' in fn):
+          dataStorage.store_installed_applications(fn)
+        elif ('endpoint' in fn):
+          dataStorage.store_endpoint_info(fn)
+        elif ('user' in fn):
+          dataStorage.store_user_info(fn)
+        elif('newservice' in fn):
+          dataStorage.store_new_service(fn)
+        elif ('hotfix' in fn):
+          dataStorage.store_hotfix_info(fn)
+        processed_files.add(new_file)
+        schedule.run_pending()
+        shutil.move(fn, 'done/' + fn)
+    except Exception as e:
+      print(f"Error: {e}")
+      time.sleep(db_interval)
+      
+def run():
+  os.makedirs('done/ready', exist_ok=True)
+  directory = 'ready'
+  pat = '*.log'
+  monitor_directory(directory, pat)
+
+run()
--- Linux/storedata.py
+++ Linux/storedata.py
@@ -13,7 +13,7 @@ class StoreData:
     self.db = config.get('Database', 'DatabaseName', fallback='opendr')
     self.user = config.get('Database', 'AgentUserName', fallback='agent')
     self.password = config.get('Database', 'AgentPassword')
-    self.sslmode = 'prefer' # 'verify-ca'
+    self.sslmode = 'verify-ca'
     self.sslrootcert = config.get('Database', 'SSLRootCert')
     self.users_log_counter = 0
     self.endpoint_log_counter = 0
@@ -149,22 +149,6 @@ class StoreData:
           data = self.find_pattern(line)
           final_params = [data.get('timestamp'), data.get('hostname'), '', '', '', data.get('name'), '', data.get('description'), '', data.get('username'), '', data.get('sid')]
           fillers = ("%s," * 12)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
-
-  def store_defender_info(self, filename):
-    table = 'systemalerts(timestmp, event, username, title, severity, category, executable, filepath, eventid, threatid, origin, type, source, description, reference, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'), data.get('event'), data.get('username'), data.get('title'), data.get('severity'), data.get('category'), data.get('executable'),
-                          data.get('filepath'), data.get('eventid'), data.get('threatid'), data.get('origin'), data.get('type'), data.get('source'), data.get('description'), data.get('references'), data.get('sid')]
-          fillers = ("%s," * 16)[:-1]
           sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
           connection.execute(sqlInsertStatement, final_params)
           connection.commit()
