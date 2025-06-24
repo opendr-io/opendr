747e77c6f6b7eecac8f31786926d7e068ec63df9
anidev95 <130506893+anidev95@users.noreply.github.com>
2025-06-24 12:08:31 -0400

    Altered sensor.py to output standard output

--- Database/storedata.py
+++ Database/storedata.py
@@ -31,142 +31,169 @@ class StoreData:
 
   def store_process_events(self, filename):
     table = 'systemevents(timestmp, event, pid, name, hostname, ppid, parent, username, dnsname, dnsdate, sourceip, sourceport, destip, destport, asname, status, exe, cmdline, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'),  data.get('event'), data.get('pid'), data.get('name'), data.get('hostname'), data.get('ppid'), data.get('parent'), data.get('username')]
-          final_params[9:9] = [''] * 8
-          final_params.extend([ data.get('exe'),  data.get('cmdline'), data.get('sid') or data.get('uuid')])
-          fillers = ("%s," * 19)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
+    try:
+      with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+        with open(filename, 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+            if(not line):
+              continue
+            data = self.find_pattern(line)
+            final_params = [data.get('timestamp'),  data.get('event'), data.get('pid'), data.get('name'), data.get('hostname'), data.get('ppid'), data.get('parent'), data.get('username')]
+            final_params[9:9] = [''] * 8
+            final_params.extend([ data.get('exe'),  data.get('cmdline'), data.get('sid') or data.get('uuid')])
+            fillers = ("%s," * 19)[:-1]
+            sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+            connection.execute(sqlInsertStatement, final_params)
+            connection.commit()
+    except Exception as e:
+      print(e)
 
   def store_network_events(self, filename):
     table = 'systemevents(timestmp, event, pid, name, hostname, ppid, parent, username, dnsname, dnsdate, sourceip, sourceport, destip, destport, asname, status, exe, cmdline, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'),  data.get('event'), data.get('pid'), data.get('name'), data.get('hostname'), '', '', data.get('username'), '', '',
-          data.get('sourceip'), data.get('sourceport'), data.get('destip'), data.get('destport'), '', data.get('status'), '', '', data.get('sid') or data.get('uuid')]
-          fillers = ("%s," * 19)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
+    try:
+      with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+        with open(filename, 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+            if(not line):
+              continue
+            data = self.find_pattern(line)
+            final_params = [data.get('timestamp'),  data.get('event'), data.get('pid'), data.get('name'), data.get('hostname'), '', '', data.get('username'), '', '',
+            data.get('sourceip'), data.get('sourceport'), data.get('destip'), data.get('destport'), '', data.get('status'), '', '', data.get('sid') or data.get('uuid')]
+            fillers = ("%s," * 19)[:-1]
+            sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+            connection.execute(sqlInsertStatement, final_params)
+            connection.commit()
+    except Exception as e:
+      print(e)
 
   def store_installed_services(self, filename):
     table = 'applications(timestmp, hostname, pid, ec2instanceid, program, servicename, displayname, status, start, username, executable, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'),  data.get('hostname'), data.get('pid'), '', '', data.get('servicename'), data.get('displayname'), data.get('status'), data.get('start'),
-                          data.get('username'),  data.get('executable'), data.get('sid') or data.get('uuid')]
-          fillers = ("%s," * 12)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
+    try:
+      with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+        with open(filename, 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+            if(not line):
+              continue
+            data = self.find_pattern(line)
+            final_params = [data.get('timestamp'),  data.get('hostname'), data.get('pid'), '', '', data.get('servicename'), data.get('displayname'), data.get('status'), data.get('start'),
+                            data.get('username'),  data.get('executable'), data.get('sid') or data.get('uuid')]
+            fillers = ("%s," * 12)[:-1]
+            sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+            connection.execute(sqlInsertStatement, final_params)
+            connection.commit()
+    except Exception as e:
+      print(e)
 
   def store_installed_applications(self, filename):
     table = 'applications(timestmp, hostname, pid, ec2instanceid, program, servicename, displayname, status, start, username, executable, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'),  data.get('hostname'), '', data.get('instanceid'), data.get('program'), '', '', '', '', '', '', data.get('sid') or data.get('uuid')]
-          fillers = ("%s," * 12)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
+    try:
+      with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+        with open(filename, 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+            if(not line):
+              continue
+            data = self.find_pattern(line)
+            final_params = [data.get('timestamp'),  data.get('hostname'), '', data.get('instanceid'), data.get('program'), '', '', '', '', '', '', data.get('sid') or data.get('uuid')]
+            fillers = ("%s," * 12)[:-1]
+            sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+            connection.execute(sqlInsertStatement, final_params)
+            connection.commit()
+    except Exception as e:
+      print(e)
 
   def store_endpoint_info(self, filename):
     table = 'endpointinfo(timestmp, event, hostname, ec2instanceid, privateips, publicip, username, onterminal, fromhostname, logintime, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'), '', data.get('hostname'), data.get('ec2_instance_id'), data.get('private_ips'), data.get('public_ip'), '', '', '', '', data.get('sid') or data.get('uuid')]
-          fillers = ("%s," * 11)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
+    try:
+      with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+        with open(filename, 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+            if(not line):
+              continue
+            data = self.find_pattern(line)
+            final_params = [data.get('timestamp'), '', data.get('hostname'), data.get('ec2_instance_id'), data.get('private_ips'), data.get('public_ip'), '', '', '', '', data.get('sid') or data.get('uuid')]
+            fillers = ("%s," * 11)[:-1]
+            sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+            connection.execute(sqlInsertStatement, final_params)
+            connection.commit()
+    except Exception as e:
+      print(e)
 
   def store_user_info(self, filename):
     table = 'systemevents(timestmp, event, pid, name, hostname, ppid, parent, username, dnsname, dnsdate, sourceip, sourceport, destip, destport, asname, status, exe, cmdline, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'), data.get('event'), '', '', data.get('hostname'), '', '',
-                          data.get('username'), '', '', data.get('sourceip'), '', '', '', '', '', '', '', data.get('sid') or data.get('uuid')]
-          fillers = ("%s," * 19)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
+    try:
+      with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+        with open(filename, 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+            if(not line):
+              continue
+            data = self.find_pattern(line)
+            final_params = [data.get('timestamp'), data.get('event'), '', '', data.get('hostname'), '', '',
+                            data.get('username'), '', '', data.get('sourceip'), '', '', '', '', '', '', '', data.get('sid') or data.get('uuid')]
+            fillers = ("%s," * 19)[:-1]
+            sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+            connection.execute(sqlInsertStatement, final_params)
+            connection.commit()
+    except Exception as e:
+      print(e)
 
   def store_new_service(self, filename):
     table = 'applications(timestmp, hostname, pid, ec2instanceid, program, servicename, displayname, status, start, username, executable, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'),  data.get('hostname'), data.get('pid'), '', '', data.get('servicename'), data.get('displayname'), data.get('status'), 
-                          data.get('start'), data.get('username'),  data.get('executable'), data.get('sid') or data.get('uuid')]
-          fillers = ("%s," * 12)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
+    try:
+      with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+        with open(filename, 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+            if(not line):
+              continue
+            data = self.find_pattern(line)
+            final_params = [data.get('timestamp'),  data.get('hostname'), data.get('pid'), '', '', data.get('servicename'), data.get('displayname'), data.get('status'), 
+                            data.get('start'), data.get('username'),  data.get('executable'), data.get('sid') or data.get('uuid')]
+            fillers = ("%s," * 12)[:-1]
+            sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+            connection.execute(sqlInsertStatement, final_params)
+            connection.commit()
+    except Exception as e:
+      print(e)
 
   def store_hotfix_info(self, filename):
     table = 'applications(timestmp, hostname, pid, ec2instanceid, program, servicename, displayname, status, start, username, executable, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'), data.get('hostname'), '', '', '', data.get('name'), '', data.get('description'), '', data.get('username'), '', data.get('sid') or data.get('uuid')]
-          fillers = ("%s," * 12)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
+    try:
+      with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+        with open(filename, 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+            if(not line):
+              continue
+            data = self.find_pattern(line)
+            final_params = [data.get('timestamp'), data.get('hostname'), '', '', '', data.get('name'), '', data.get('description'), '', data.get('username'), '', data.get('sid') or data.get('uuid')]
+            fillers = ("%s," * 12)[:-1]
+            sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+            connection.execute(sqlInsertStatement, final_params)
+            connection.commit()
+    except Exception as e:
+      print(e)
 
   def store_defender_info(self, filename):
     table = 'systemalerts(timestmp, event, username, title, severity, category, executable, filepath, eventid, threatid, origin, type, source, description, reference, sid)'
-    with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
-      with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-          if(not line):
-            continue
-          data = self.find_pattern(line)
-          final_params = [data.get('timestamp'), data.get('event'), data.get('username'), data.get('title'), data.get('severity'), data.get('category'), data.get('executable'),
-                          data.get('filepath'), data.get('eventid'), data.get('threatid'), data.get('origin'), data.get('type'), data.get('source'), data.get('description'), data.get('references'), data.get('sid') or data.get('uuid')]
-          fillers = ("%s," * 16)[:-1]
-          sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
-          connection.execute(sqlInsertStatement, final_params)
-          connection.commit()
+    try:
+      with psycopg.connect(host=self.host, port=self.port, dbname=self.db, user=self.user, password=self.password, sslmode=self.sslmode, sslrootcert=self.sslrootcert) as connection:
+        with open(filename, 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+            if(not line):
+              continue
+            data = self.find_pattern(line)
+            final_params = [data.get('timestamp'), data.get('event'), data.get('username'), data.get('title'), data.get('severity'), data.get('category'), data.get('executable'),
+                            data.get('filepath'), data.get('eventid'), data.get('threatid'), data.get('origin'), data.get('type'), data.get('source'), data.get('description'), data.get('references'), data.get('sid') or data.get('uuid')]
+            fillers = ("%s," * 16)[:-1]
+            sqlInsertStatement = 'INSERT INTO ' + table + ' VALUES('+fillers+')'
+            connection.execute(sqlInsertStatement, final_params)
+            connection.commit()
+    except Exception as e:
+      print(e)
--- sensor.py
+++ sensor.py
@@ -1,32 +1,50 @@
-import subprocess
-import concurrent.futures
 import configparser
 import pathlib
+import subprocess
+import concurrent.futures
+import os
+import threading
 
 config = configparser.ConfigParser()
 config.read(pathlib.Path(__file__).parent.absolute() / "agentconfig.ini")
 
-def execute_scripts(script):
-    print(script)
-    result = subprocess.run(['python', script], capture_output=True, text=True)
-    return script, result.stdout, result.stderr
+def execute_scripts_realtime(script):
+  print(f"Starting execution of: {script}")
+  try:
+    process = subprocess.Popen(
+      ['python', script],
+      stdout=subprocess.PIPE,
+      stderr=subprocess.STDOUT,  # Combine stderr with stdout
+      text=True,
+      bufsize=1,
+      universal_newlines=True
+    )
+      
+    output_lines = []
+    # Read output line by line in real-time
+    for line in iter(process.stdout.readline, ''):
+      line = line.rstrip()
+      print(f"[{script}] {line}")
+      output_lines.append(line)
+    
+    process.wait()  # Wait for process to complete
+    return script, '\n'.join(output_lines), "", process.returncode
+  except Exception as e:
+    print(f"Error executing {script}: {e}")
+    return script, "", str(e), -1
 
 def run():
   os_mode = config.get('General', 'OperatingSystem', fallback='Windows')
   pathsep = '\\' if os_mode == 'Windows' else '/'
   generators = [os_mode + pathsep + script for script in config.get(os_mode, 'Scripts', fallback='').split(', ')]
-  # this section governs local vs database mode - default is local
-  if config.getboolean(os_mode, 'RunDatabaseOperations', fallback=False):
+  if(config.getboolean(os_mode, 'RunDatabaseOperations', fallback=False)):
     generators.append('Database' + pathsep + 'dboperations.py')
 
   print('Starting Scripts')
+  print(f"Will execute: {generators}")
+  
   with concurrent.futures.ThreadPoolExecutor(len(generators)) as executor:
-    results = executor.map(execute_scripts, generators)
-    for script, stdout, stderr in results:
-      print(f"Results from {script}:")
-      print(stdout)
-      if(stderr):
-        print(f"Errors from {script}:")
-        print(stderr)
-
+    results = executor.map(execute_scripts_realtime, generators)
+    for script, stdout, stderr, returncode in results:
+      print(f"\nCompleted {script} with return code: {returncode}")
 run()
