da176631ccaca5a452b66eb3f1457f24af11ed8d
anidev95 <130506893+anidev95@users.noreply.github.com>
2025-10-14 12:16:02 -0400

    Update network-aug.py
    Signed-off-by: anidev95 <130506893+anidev95@users.noreply.github.com>

--- Augment/network-aug.py
+++ Augment/network-aug.py
@@ -18,7 +18,7 @@ def fetch_unique_ips(interval):
     with psycopg.connect(host=config.get('Database', 'HostName'), port=config.get('Database', 'PortNumber', fallback='4000'), dbname=config.get('Database', 'DatabaseName', fallback='opendr'),
                         user=config.get('Database', 'RootDatabaseUserName', fallback='postgres'), password=config.get('Database', 'RootDatabasePassword'), autocommit=True) as connection:
         with connection.cursor() as cursor:
-            ips = cursor.execute(f"SELECT DISTINCT(sourceip) FROM systemevents WHERE timestmp > '{(datetime.now() - timedelta(seconds=interval)).strftime('%Y-%m-%d %H:%M:%S')}'").fetchall()
+            ips = cursor.execute(f"SELECT DISTINCT(sourceip) FROM systemevents WHERE timestamp > '{(datetime.now() - timedelta(seconds=interval)).strftime('%Y-%m-%d %H:%M:%S')}'").fetchall()
         connection.close()
     return [ip[0] for ip in ips]
 
@@ -26,7 +26,7 @@ def enrich_network(ip, dns_name, as_name, interval):
     with psycopg.connect(host=config.get('Database', 'HostName'), port=config.get('Database', 'PortNumber', fallback='4000'), dbname=config.get('Database', 'DatabaseName', fallback='opendr'),
                         user=config.get('Database', 'RootDatabaseUserName', fallback='postgres'), password=config.get('Database', 'RootDatabasePassword'), autocommit=True) as connection:
         with connection.cursor() as cursor:
-            cursor.execute(f"UPDATE systemevents SET dnsname = '{dns_name}', asname = '{as_name}' WHERE sourceip = '{ip}' AND timestmp > '{(datetime.now() - timedelta(seconds=interval)).strftime('%Y-%m-%d %H:%M:%S')}'")
+            cursor.execute(f"UPDATE systemevents SET dnsname = '{dns_name}', asname = '{as_name}' WHERE sourceip = '{ip}' AND timestamp > '{(datetime.now() - timedelta(seconds=interval)).strftime('%Y-%m-%d %H:%M:%S')}'")
         connection.commit()
 
 def run():
